da7f590825056dee43a5e1b39d2b51c6daad3b9a
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-17 00:46:46 +0100

    getting to understand betty

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (sucess)
+ *
+ * sizeof - to check the address in memory of various data type
+ *
+ * Description: This prints the size of various data type
+ * on the computer it is compiled and run on.
+ */
+int main(void)
+{
+		char p;
+		int i;
+		long int li;
+		long long int lli;
+		float ft;
+
+		printf("Size of a char: %ld byte(s)\n", sizeof(p));
+		printf("Size of a int: %ld byte(s)\n", sizeof(i));
+		printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+		printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+		return (0);
+}
