fc815bb587a4fcc88c3672c53773aeba84d883d4
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-21 16:50:30 +0100

    function test for alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,13 @@
 /**
  * _isalpha - checks for alphabetic characters
  * @c: charater to test
+ *
+ * Return: return 1 for success
  */
 int _isalpha(int c)
 {
-    if((c >= '\x61' || c > = '\x41') && (c <= '\x7A' || c <= '\x5A'))
-        return (1);
-    else
-        return (0);
+	if ((c >= '\x61' || c >= '\x41') && (c <= '\x7A' || c <= '\x5A'))
+		return (1);
+	else
+		return (0);
 }
