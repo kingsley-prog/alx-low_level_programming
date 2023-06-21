17f5912c0aeb271adbf8d8531ff7249e04ca6f39
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-21 15:18:36 +0100

    print lower case using two loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - print 10 times lowercase letter with newline
  */
-void print_alphabet_x10
+void print_alphabet_x10(void)
 {
 	char c;
 	int i;
@@ -10,7 +10,9 @@ void print_alphabet_x10
 	for (i = 0; i < 10; i++)
 	{
 		for (c = '\x61'; c <= '\x7A'; c++)
-			_putchar(c);
+        {
+            _putchar(c);
+        }
 		_putchar('\x0A');
 
 	}
