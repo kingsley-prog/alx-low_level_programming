0caa3c597feadfcfe400fa94f85c5d87db37ddf5
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-21 23:12:23 +0100

    make it abosolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of integer
+ *
+ * Return: return the absolutr value of the integer
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+		return (i);
+	}
+	else
+		return (i);
+}
