938c756a588f0946ba0f9f1a378a8d8eeef865e2
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-22 12:19:35 +0100

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			/* get the first number with / and the second with % */
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+
+			/* get the other part first and second with / and % */
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		i++
+	}
+}
