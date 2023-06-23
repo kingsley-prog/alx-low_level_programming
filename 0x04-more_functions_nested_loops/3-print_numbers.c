df17b098b2931e37423d9bfab5a2900d724325ca
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-23 17:12:22 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - print number from 0 to 9 then newline
+ */
+void print_numbers(void)
+{
+	int i;
+	for(i = 0; i <= 9; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
