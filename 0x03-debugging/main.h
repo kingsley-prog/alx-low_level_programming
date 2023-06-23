ad67cac98d2f21663deb86561ed706f38ad33519
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-23 01:01:00 +0100

    test my function

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,8 @@
-#ifndef MAiN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void jack_bauer(void);
-int add(int, int);
-void print_to_98(int n);
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
