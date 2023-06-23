9a356fb006331048c26b982bd4dd961890d643c9
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-23 18:11:24 +0100

    skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers form 0 to 9
+ *
+ * Constraint: do not print 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for(c = 48; c <= 57; c++)
+	{
+		if (c == 50 || c == 52)
+			continue;
+		_putchar('0' + c);
+	}
+	_putchar('\n');
+}
