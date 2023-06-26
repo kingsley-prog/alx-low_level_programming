011d03b83e23565f795f451ad2bf2354029c89b5
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-26 19:20:24 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse string
+ * @s: a pointer to a char
+ */
+
+void print_rev(char *s)
+{
+    int i = _strlen(s);
+
+    for (i = _strlen(s); *(str + i) != 0; i--)
+    {
+        _putchar(str[i];
+    }
+    _putchar('\n');
+}
