ff3a56df4a09e96ac53144437aa57f68bb9bcfd5
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-26 20:37:34 +0100

    reverse 2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: pointer to char(the string)
+ */
+
+void rev_string(char *s)
+{
+    int i;
+
+    i = 0;
+    while (*(s + i) != 0)
+        i++;
+    while (i--)
+        _putchar(s[i]);
+    _putchar('\0');
+}
