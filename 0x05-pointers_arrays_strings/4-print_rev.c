624dc3ccf037013bb5036153a278b02b18b4a9c2
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-26 19:25:47 +0100

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,9 @@ void print_rev(char *s)
 {
     int i = _strlen(s);
 
-    for (i = _strlen(s); *(str + i) != 0; i--)
+    for (i = _strlen(s); *(s + i) != 0; i--)
     {
-        _putchar(str[i];
+        _putchar(s[i]);
     }
     _putchar('\n');
 }
