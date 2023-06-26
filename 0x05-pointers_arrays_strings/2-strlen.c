39ebf993faa3b9a502bc3f27ac91e9a1ecb01f96
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-26 16:26:29 +0100

    count lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - return the length of string
+ * @s: a pointer to char type
+ *
+ * Return: count of char;
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != 0; i++)
+
+	return (i);
+}
