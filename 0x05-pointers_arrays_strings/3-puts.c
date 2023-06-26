1235bf50f707b65ed03ba8bb017f030de67d0d5d
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-26 19:09:36 +0100

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,7 +8,11 @@ void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i) != 0; i++)
-		_putchar(str[i]);
+	i = 0;
+
+	while (*(str + i) != 0)
+	{
+		_putchar(*(str + i));
+	}
 	_putchar('\n');
 }
