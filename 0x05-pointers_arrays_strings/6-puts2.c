e8694506765336457ceda2a2e40feddc20b8f712
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-27 03:26:21 +0100

    even string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print even values in strings
+ * @str: value to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
