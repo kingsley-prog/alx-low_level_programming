8deccaea08b199f60e8b1c1b9ecb3e76399892cc
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-30 19:51:52 -0700

    use if

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
 	for (c = 48; c <= 57; c++)
 	{
 		if (c != 50 && c != 52)
-			_putchar('0' + c);
+			_putchar(c);
 	}
 	_putchar('\n');
 }
