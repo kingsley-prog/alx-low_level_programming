4d0a42340e29c39115871c7cc32ed4360e967796
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-30 17:47:00 -0700

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _strcmp - compare two strings
+  * @s1: first char pointer
+  * @s2: second char pointer
+  *
+  * Return: return the diff of the two strings
+  */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
