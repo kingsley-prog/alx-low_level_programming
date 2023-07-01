55e63fac500188b232ff766394ff32e0ea6d7039
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-30 18:58:36 -0700

    added if conditoion

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,14 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0' && s[i] >= 'a' && s[i] <= 'z')
+	while (s[i] != '\0')
 	{
-		s[i] = s[i] - 32;
-		i++;
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+
+			s[i] = s[i] - 32;
+			i++;
+		}
 	}
 	return (s);
 }
