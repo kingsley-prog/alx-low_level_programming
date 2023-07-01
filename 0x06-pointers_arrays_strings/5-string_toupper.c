859836fdfa555a09b3cc240993bade4d26cf62fd
kingsley-prog <kingsley_lawani@outlook.com>
2023-06-30 18:48:09 -0700

    subracr 32 from lower cae ascii value to get upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * string_toupper - to upper case letters
+  * @s: a pointer to a char type
+  *
+  * Return: return a upper case letters
+  */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] >== 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
