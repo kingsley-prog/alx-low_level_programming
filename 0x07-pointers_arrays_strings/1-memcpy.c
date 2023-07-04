5c8b4b2bd9512b398bc20bc5fb82e89aa9fe0273
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-04 03:46:53 +0100

    betty issues corrected

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,16 +2,16 @@
 /**
  * _memcpy - copies memory area
  * @dest: char pointer
- * @scr: string to copy
+ * @src: string to copy
  * @n: bytes to copy
  *
  * Return: dest a char pointer
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        dest[i] = src[i];
-    return (dest);
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
 }
