7644d910fce9371b68989048b8e048eeb3cfde8f
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-04 04:56:34 +0100

    increment

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,6 +18,7 @@ char *_strchr(char *s, char c)
 			/* pointer arithemetic */
 			s = s + i;
 			return (s);
+			i++;
 		}
 	}
 	return (NULL);
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @a: substrings
+ *
+ * Return: returns an int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
