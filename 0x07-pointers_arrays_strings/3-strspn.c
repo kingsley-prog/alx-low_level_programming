00ce73775c48d0b05e99f34437ded04715e47201
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-04 05:26:51 +0100

    cool

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,11 +2,29 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string
- * @a: substrings
+ * @accept: substrings
  *
  * Return: returns an int
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j, c;
 
+	c = 0;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		j = 0;
+		while (accept[j] != 0)
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
 }
