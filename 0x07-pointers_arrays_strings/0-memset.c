8c1779b7e4dc8d1c220a6d142b4b3f68ee4b01a1
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-04 01:10:40 +0100

    address betty issues

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,19 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: area in memory(a pointer to a char data type)
- * @b: constant byte(char data  type)
+ * @b: constant byte(char data	type)
  * @n: range in memory to fill in constant byte
+ *
+ * Return: return the string filled with contant byte
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    if (s)
-    {
-        int i = 0
-        while (i < n)
-            s[i] = b;
-    }
-    return (s);
+	if (s)
+	{
+		int i = 0
+
+		while (i < n)
+			s[i] = b;
+	}
+	return (s);
 }
