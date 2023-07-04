d060c00b1634d0a740a19f161e23e6dbbf0ab503
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-04 03:25:04 +0100

    test in while loop

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 		unsigned int i = 0
 
-		while (i < n && s != 0)
+		while (i < n)
 		{
 			s[i] = b;
 			i++;
 		}
-	s[i] = '\0';
 	return (s);
 }
