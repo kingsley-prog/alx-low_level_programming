ac9fddf213d222b7d1e44d8da9d9a8becf4dd72b
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-04 01:07:58 +0100

    contant memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: area in memory(a pointer to a char data type)
+ * @b: constant byte(char data  type)
+ * @n: range in memory to fill in constant byte
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    if (s)
+    {
+        int i = 0
+        while (i < n)
+            s[i] = b;
+    }
+    return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -50,4 +50,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+char *_memset(char *s, char b, unsigned int n);
 #endif
