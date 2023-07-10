9eea815262a73f35dc0d49c5962cb2682957f86c
kingsley-prog <kingsley_lawani@outlook.com>
2023-07-10 08:41:47 +0100

    count argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argumnent vector
+ *
+ * Return: success
+ * Description: prints the number of arguments passed to it
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	int i;
+
+	i = 1;
+
+	while (i < argc)
+	{
+		i++;
+	}
+	i--;
+	if (i >= 10)
+	{
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+	else
+		_putchar(i + '0');
+	_putchar('\n');
+
+	return (0);
+}
